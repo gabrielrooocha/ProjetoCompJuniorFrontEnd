2f425fd03cd2eecf18a59753433be6443b44f032
Gabriel <gabrielrocha.almeida2006@gmail.com>
2025-11-20 11:54:31 -0300

    att

--- projetocomp/src/pages/Principal/Principal.js
+++ projetocomp/src/pages/Principal/Principal.js
@@ -1,37 +1,58 @@
-import React from "react";
-import styles from './Principal.module.css'
-import { useState } from "react";
+// CSS
+import styles from "./Principal.module.css";
+
+// hooks
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import { useNavigate, Link } from "react-router-dom";
 
-const Principal = () => {
-    const [query, setQuery] = useState("");
-    const [posts] = useState([])
+// react
+import { useState } from "react";
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+// components
+import PostDetail from "../../components/PostDetail";
+
+const Principal = () => {
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
+
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
     }
-    return (
-        <div className={styles.home}>
-            <h1>
-                Veja os nossos posts mais recentes
-            </h1>
-            <form onSubmit={handleSubmit} className={styles.search_form}>
-                <input type = "text" placeholder="Ou busque por tags"
-                onChange={(e) => setQuery(e.target.value)}
-                />
-                <button className = "btn btn-dark">Pesquisar</button>
-            </form>
-            <div>
-                <h1>Posts...</h1>
-                {posts && posts.length === 0 &&(
-                    <div className = {styles.noposts}>
-                        <p>Não foram encontrados posts</p>
-                        <Link to="/posts/create">Criar primeiro post</Link>
-                        </div>
-                )}
-            </div>
-        </div>
-    );
+  };
+
+  console.log(loading);
+
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form className={styles.search_form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div className="post-list">
+        {loading && <p>Carregando...</p>}
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => <PostDetail key={post.id} post={post} />)}
+      </div>
+    </div>
+  );
 };
 
 export default Principal;
